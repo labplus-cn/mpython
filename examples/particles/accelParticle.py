2586dba51b341fccea3370153b2c4390b7766a7f
pydo <964683112@qq.com>
2018-10-18 13:30:37 +0800

    格式美化

--- examples/particles/accelParticle.py
+++ examples/particles/accelParticle.py
@@ -8,46 +8,51 @@ from time import sleep_ms
 from random import randint
 from collections import deque
 
+
 class Ball():
     '''
     粒子类
     '''
-    def __init__(self,x,y):
+
+    def __init__(self, x, y):
         self.x = x
         self.y = y
-        
-        self.vx = randint(-6,6)
-        self.vy = randint(-6,6)
-        
+
+        self.vx = randint(-6, 6)
+        self.vy = randint(-6, 6)
+
     def run(self):
         self.update()
         self.display()
-        
+
     def update(self):
-       
-        
+
         self.x += self.vx
         self.y += self.vy
+
     def isDead(self):
 
         if self.x > 128 or self.x < 0 or self.y > 64 or self.y < 0:
             return True
         else:
             return False
-            
+
     def display(self):
-        display.pixel(self.x,self.y,1)
+        display.pixel(self.x, self.y, 1)
+
 
 class ParticleSystem():
     '''
     粒子系统类
     '''
-    def __init__(self,x,y):
+
+    def __init__(self, x, y):
         self.particles = []
         self.x = x
         self.y = y
         for i in range(200):
             self.particles.append(Ball(self.x, self.y))
+
     def run(self):
         for p in self.particles:
             if p.isDead():
@@ -55,24 +60,21 @@ class ParticleSystem():
                 p.y = self.y
             p.run()
 
-# 初始化粒子系统        
-ps = ParticleSystem(0,0)
+
+# 初始化粒子系统
+ps = ParticleSystem(0, 0)
 
 while True:
-    
+
     sleep_ms(20)
 
     display.fill(0)
-    
-    
+
     x = accelerometer.get_y()
     y = accelerometer.get_x()
     ps.x = 128-int(x*64+64)
     ps.y = int(y*32+32)
     ps.run()
     # 把加速度计的读数
-    
-    display.show()
 
-    
-    
+    display.show()
