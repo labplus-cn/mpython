cfd35a102605cc648c14f7b43e891d64668c4530
lgnativs <apple_eat@126.com>
2018-10-25 14:12:10 +0800

    提交掌控板固件源码

---
+++ mpy-cross/build/genhdr/qstrdefs.preprocessed.h
@@ -0,0 +1,1104 @@
+# 1 "<stdin>"
+# 1 "<built-in>"
+# 1 "<command-line>"
+# 1 "/usr/include/stdc-predef.h" 1 3 4
+# 1 "<command-line>" 2
+# 1 "<stdin>"
+# 27 "<stdin>"
+# 1 "../py/mpconfig.h" 1
+# 45 "../py/mpconfig.h"
+# 1 "./mpconfigport.h" 1
+# 91 "./mpconfigport.h"
+typedef long mp_int_t;
+typedef unsigned long mp_uint_t;
+# 108 "./mpconfigport.h"
+typedef long mp_off_t;
+# 123 "./mpconfigport.h"
+# 1 "/usr/include/alloca.h" 1 3 4
+# 21 "/usr/include/alloca.h" 3 4
+# 1 "/usr/include/features.h" 1 3 4
+# 424 "/usr/include/features.h" 3 4
+# 1 "/usr/include/x86_64-linux-gnu/sys/cdefs.h" 1 3 4
+# 427 "/usr/include/x86_64-linux-gnu/sys/cdefs.h" 3 4
+# 1 "/usr/include/x86_64-linux-gnu/bits/wordsize.h" 1 3 4
+# 428 "/usr/include/x86_64-linux-gnu/sys/cdefs.h" 2 3 4
+# 1 "/usr/include/x86_64-linux-gnu/bits/long-double.h" 1 3 4
+# 429 "/usr/include/x86_64-linux-gnu/sys/cdefs.h" 2 3 4
+# 425 "/usr/include/features.h" 2 3 4
+# 448 "/usr/include/features.h" 3 4
+# 1 "/usr/include/x86_64-linux-gnu/gnu/stubs.h" 1 3 4
+# 10 "/usr/include/x86_64-linux-gnu/gnu/stubs.h" 3 4
+# 1 "/usr/include/x86_64-linux-gnu/gnu/stubs-64.h" 1 3 4
+# 11 "/usr/include/x86_64-linux-gnu/gnu/stubs.h" 2 3 4
+# 449 "/usr/include/features.h" 2 3 4
+# 22 "/usr/include/alloca.h" 2 3 4
+
+
+# 1 "/usr/lib/gcc/x86_64-linux-gnu/7/include/stddef.h" 1 3 4
+# 216 "/usr/lib/gcc/x86_64-linux-gnu/7/include/stddef.h" 3 4
+
+# 216 "/usr/lib/gcc/x86_64-linux-gnu/7/include/stddef.h" 3 4
+typedef long unsigned int size_t;
+# 25 "/usr/include/alloca.h" 2 3 4
+
+
+
+
+
+
+
+extern void *alloca (size_t __size) __attribute__ ((__nothrow__ , __leaf__));
+
+
+
+
+
+
+# 124 "./mpconfigport.h" 2
+
+
+# 1 "/usr/lib/gcc/x86_64-linux-gnu/7/include/stdint.h" 1 3 4
+# 9 "/usr/lib/gcc/x86_64-linux-gnu/7/include/stdint.h" 3 4
+# 1 "/usr/include/stdint.h" 1 3 4
+# 26 "/usr/include/stdint.h" 3 4
+# 1 "/usr/include/x86_64-linux-gnu/bits/libc-header-start.h" 1 3 4
+# 27 "/usr/include/stdint.h" 2 3 4
+# 1 "/usr/include/x86_64-linux-gnu/bits/types.h" 1 3 4
+# 27 "/usr/include/x86_64-linux-gnu/bits/types.h" 3 4
+# 1 "/usr/include/x86_64-linux-gnu/bits/wordsize.h" 1 3 4
+# 28 "/usr/include/x86_64-linux-gnu/bits/types.h" 2 3 4
+
+
+typedef unsigned char __u_char;
+typedef unsigned short int __u_short;
+typedef unsigned int __u_int;
+typedef unsigned long int __u_long;
+
+
+typedef signed char __int8_t;
+typedef unsigned char __uint8_t;
+typedef signed short int __int16_t;
+typedef unsigned short int __uint16_t;
+typedef signed int __int32_t;
+typedef unsigned int __uint32_t;
+
+typedef signed long int __int64_t;
+typedef unsigned long int __uint64_t;
+
+
+
+
+
+
+
+typedef long int __quad_t;
+typedef unsigned long int __u_quad_t;
+
+
+
+
+
+
+
+typedef long int __intmax_t;
+typedef unsigned long int __uintmax_t;
+# 130 "/usr/include/x86_64-linux-gnu/bits/types.h" 3 4
+# 1 "/usr/include/x86_64-linux-gnu/bits/typesizes.h" 1 3 4
+# 131 "/usr/include/x86_64-linux-gnu/bits/types.h" 2 3 4
+
+
+typedef unsigned long int __dev_t;
+typedef unsigned int __uid_t;
+typedef unsigned int __gid_t;
+typedef unsigned long int __ino_t;
+typedef unsigned long int __ino64_t;
+typedef unsigned int __mode_t;
+typedef unsigned long int __nlink_t;
+typedef long int __off_t;
+typedef long int __off64_t;
+typedef int __pid_t;
+typedef struct { int __val[2]; } __fsid_t;
+typedef long int __clock_t;
+typedef unsigned long int __rlim_t;
+typedef unsigned long int __rlim64_t;
+typedef unsigned int __id_t;
+typedef long int __time_t;
+typedef unsigned int __useconds_t;
+typedef long int __suseconds_t;
+
+typedef int __daddr_t;
+typedef int __key_t;
+
+
+typedef int __clockid_t;
+
+
+typedef void * __timer_t;
+
+
+typedef long int __blksize_t;
+
+
+
+
+typedef long int __blkcnt_t;
+typedef long int __blkcnt64_t;
+
+
+typedef unsigned long int __fsblkcnt_t;
+typedef unsigned long int __fsblkcnt64_t;
+
+
+typedef unsigned long int __fsfilcnt_t;
+typedef unsigned long int __fsfilcnt64_t;
+
+
+typedef long int __fsword_t;
+
+typedef long int __ssize_t;
+
+
+typedef long int __syscall_slong_t;
+
+typedef unsigned long int __syscall_ulong_t;
+
+
+
+typedef __off64_t __loff_t;
+typedef char *__caddr_t;
+
+
+typedef long int __intptr_t;
+
+
+typedef unsigned int __socklen_t;
+
+
+
+
+typedef int __sig_atomic_t;
+# 28 "/usr/include/stdint.h" 2 3 4
+# 1 "/usr/include/x86_64-linux-gnu/bits/wchar.h" 1 3 4
+# 29 "/usr/include/stdint.h" 2 3 4
+# 1 "/usr/include/x86_64-linux-gnu/bits/wordsize.h" 1 3 4
+# 30 "/usr/include/stdint.h" 2 3 4
+
+
+
+
+# 1 "/usr/include/x86_64-linux-gnu/bits/stdint-intn.h" 1 3 4
+# 24 "/usr/include/x86_64-linux-gnu/bits/stdint-intn.h" 3 4
+typedef __int8_t int8_t;
+typedef __int16_t int16_t;
+typedef __int32_t int32_t;
+typedef __int64_t int64_t;
+# 35 "/usr/include/stdint.h" 2 3 4
+
+
+# 1 "/usr/include/x86_64-linux-gnu/bits/stdint-uintn.h" 1 3 4
+# 24 "/usr/include/x86_64-linux-gnu/bits/stdint-uintn.h" 3 4
+typedef __uint8_t uint8_t;
+typedef __uint16_t uint16_t;
+typedef __uint32_t uint32_t;
+typedef __uint64_t uint64_t;
+# 38 "/usr/include/stdint.h" 2 3 4
+
+
+
+
+
+typedef signed char int_least8_t;
+typedef short int int_least16_t;
+typedef int int_least32_t;
+
+typedef long int int_least64_t;
+
+
+
+
+
+
+typedef unsigned char uint_least8_t;
+typedef unsigned short int uint_least16_t;
+typedef unsigned int uint_least32_t;
+
+typedef unsigned long int uint_least64_t;
+# 68 "/usr/include/stdint.h" 3 4
+typedef signed char int_fast8_t;
+
+typedef long int int_fast16_t;
+typedef long int int_fast32_t;
+typedef long int int_fast64_t;
+# 81 "/usr/include/stdint.h" 3 4
+typedef unsigned char uint_fast8_t;
+
+typedef unsigned long int uint_fast16_t;
+typedef unsigned long int uint_fast32_t;
+typedef unsigned long int uint_fast64_t;
+# 97 "/usr/include/stdint.h" 3 4
+typedef long int intptr_t;
+
+
+typedef unsigned long int uintptr_t;
+# 111 "/usr/include/stdint.h" 3 4
+typedef __intmax_t intmax_t;
+typedef __uintmax_t uintmax_t;
+# 10 "/usr/lib/gcc/x86_64-linux-gnu/7/include/stdint.h" 2 3 4
+# 126 "./mpconfigport.h" 2
+# 46 "../py/mpconfig.h" 2
+# 582 "../py/mpconfig.h"
+
+# 582 "../py/mpconfig.h"
+typedef double mp_float_t;
+# 28 "<stdin>" 2
+
+
+
+
+
+QCFG(BYTES_IN_LEN, (1))
+QCFG(BYTES_IN_HASH, (2))
+
+Q()
+Q(*)
+Q(_)
+Q(/)
+Q(%#o)
+Q(%#x)
+Q({:#b})
+Q( )
+Q(\n)
+Q(maximum recursion depth exceeded)
+Q(<module>)
+Q(<lambda>)
+Q(<listcomp>)
+Q(<dictcomp>)
+Q(<setcomp>)
+Q(<genexpr>)
+Q(<string>)
+Q(<stdin>)
+Q(utf-8)
+
+
+
+
+
+Q(ArithmeticError)
+
+Q(ArithmeticError)
+
+Q(AssertionError)
+
+Q(AssertionError)
+
+Q(AssertionError)
+
+Q(AttributeError)
+
+Q(AttributeError)
+
+Q(BaseException)
+
+Q(BaseException)
+
+Q(EOFError)
+
+Q(EOFError)
+
+Q(Ellipsis)
+
+Q(Ellipsis)
+
+Q(Exception)
+
+Q(Exception)
+
+Q(Exception)
+
+Q(GeneratorExit)
+
+Q(GeneratorExit)
+
+Q(ImportError)
+
+Q(ImportError)
+
+Q(IndentationError)
+
+Q(IndentationError)
+
+Q(IndexError)
+
+Q(IndexError)
+
+Q(KeyError)
+
+Q(KeyError)
+
+Q(KeyboardInterrupt)
+
+Q(KeyboardInterrupt)
+
+Q(LookupError)
+
+Q(LookupError)
+
+Q(MemoryError)
+
+Q(MemoryError)
+
+Q(NameError)
+
+Q(NameError)
+
+Q(NoneType)
+
+Q(NotImplementedError)
+
+Q(NotImplementedError)
+
+Q(OSError)
+
+Q(OSError)
+
+Q(OverflowError)
+
+Q(OverflowError)
+
+Q(RuntimeError)
+
+Q(RuntimeError)
+
+Q(StopAsyncIteration)
+
+Q(StopAsyncIteration)
+
+Q(StopAsyncIteration)
+
+Q(StopIteration)
+
+Q(StopIteration)
+
+Q(SyntaxError)
+
+Q(SyntaxError)
+
+Q(SystemExit)
+
+Q(SystemExit)
+
+Q(TypeError)
+
+Q(TypeError)
+
+Q(UnicodeError)
+
+Q(UnicodeError)
+
+Q(ValueError)
+
+Q(ValueError)
+
+Q(ZeroDivisionError)
+
+Q(ZeroDivisionError)
+
+Q(_0x0a_)
+
+Q(__add__)
+
+Q(__aenter__)
+
+Q(__aenter__)
+
+Q(__aexit__)
+
+Q(__aexit__)
+
+Q(__aiter__)
+
+Q(__anext__)
+
+Q(__bool__)
+
+Q(__build_class__)
+
+Q(__call__)
+
+Q(__class__)
+
+Q(__class__)
+
+Q(__class__)
+
+Q(__class__)
+
+Q(__class__)
+
+Q(__class__)
+
+Q(__class__)
+
+Q(__contains__)
+
+Q(__contains__)
+
+Q(__delitem__)
+
+Q(__delitem__)
+
+Q(__dict__)
+
+Q(__enter__)
+
+Q(__eq__)
+
+Q(__eq__)
+
+Q(__exit__)
+
+Q(__ge__)
+
+Q(__getattr__)
+
+Q(__getattr__)
+
+Q(__getitem__)
+
+Q(__getitem__)
+
+Q(__getitem__)
+
+Q(__getitem__)
+
+Q(__gt__)
+
+Q(__hash__)
+
+Q(__iadd__)
+
+Q(__import__)
+
+Q(__init__)
+
+Q(__init__)
+
+Q(__init__)
+
+Q(__isub__)
+
+Q(__iter__)
+
+Q(__le__)
+
+Q(__len__)
+
+Q(__lt__)
+
+Q(__main__)
+
+Q(__main__)
+
+Q(__main__)
+
+Q(__main__)
+
+Q(__main__)
+
+Q(__main__)
+
+Q(__main__)
+
+Q(__module__)
+
+Q(__name__)
+
+Q(__name__)
+
+Q(__name__)
+
+Q(__name__)
+
+Q(__name__)
+
+Q(__name__)
+
+Q(__name__)
+
+Q(__name__)
+
+Q(__name__)
+
+Q(__name__)
+
+Q(__name__)
+
+Q(__new__)
+
+Q(__new__)
+
+Q(__new__)
+
+Q(__next__)
+
+Q(__next__)
+
+Q(__next__)
+
+Q(__next__)
+
+Q(__path__)
+
+Q(__path__)
+
+Q(__path__)
+
+Q(__qualname__)
+
+Q(__repl_print__)
+
+Q(__repl_print__)
+
+Q(__repr__)
+
+Q(__repr__)
+
+Q(__reversed__)
+
+Q(__setitem__)
+
+Q(__setitem__)
+
+Q(__str__)
+
+Q(__sub__)
+
+Q(__traceback__)
+
+Q(_brace_open__colon__hash_b_brace_close_)
+
+Q(_lt_dictcomp_gt_)
+
+Q(_lt_genexpr_gt_)
+
+Q(_lt_lambda_gt_)
+
+Q(_lt_listcomp_gt_)
+
+Q(_lt_module_gt_)
+
+Q(_lt_setcomp_gt_)
+
+Q(_lt_string_gt_)
+
+Q(_percent__hash_o)
+
+Q(_percent__hash_x)
+
+Q(_space_)
+
+Q(_star_)
+
+Q(_star_)
+
+Q(abs)
+
+Q(add)
+
+Q(all)
+
+Q(any)
+
+Q(append)
+
+Q(append)
+
+Q(args)
+
+Q(bin)
+
+Q(bool)
+
+Q(bool)
+
+Q(bound_method)
+
+Q(builtins)
+
+Q(builtins)
+
+Q(bytearray)
+
+Q(bytearray)
+
+Q(bytecode)
+
+Q(bytes)
+
+Q(bytes)
+
+Q(bytes)
+
+Q(calcsize)
+
+Q(callable)
+
+Q(chr)
+
+Q(classmethod)
+
+Q(classmethod)
+
+Q(clear)
+
+Q(clear)
+
+Q(clear)
+
+Q(close)
+
+Q(close)
+
+Q(closure)
+
+Q(closure)
+
+Q(complex)
+
+Q(complex)
+
+Q(const)
+
+Q(const)
+
+Q(copy)
+
+Q(copy)
+
+Q(copy)
+
+Q(count)
+
+Q(count)
+
+Q(count)
+
+Q(count)
+
+Q(decode)
+
+Q(default)
+
+Q(delattr)
+
+Q(deleter)
+
+Q(dict)
+
+Q(dict)
+
+Q(dict_view)
+
+Q(difference)
+
+Q(difference_update)
+
+Q(dir)
+
+Q(discard)
+
+Q(divmod)
+
+Q(doc)
+
+Q(encode)
+
+Q(end)
+
+Q(endswith)
+
+Q(endswith)
+
+Q(enumerate)
+
+Q(enumerate)
+
+Q(eval)
+
+Q(exec)
+
+Q(extend)
+
+Q(extend)
+
+Q(filter)
+
+Q(filter)
+
+Q(find)
+
+Q(find)
+
+Q(float)
+
+Q(float)
+
+Q(format)
+
+Q(format)
+
+Q(from_bytes)
+
+Q(fromkeys)
+
+Q(function)
+
+Q(function)
+
+Q(function)
+
+Q(function)
+
+Q(function)
+
+Q(function)
+
+Q(function)
+
+Q(function)
+
+Q(function)
+
+Q(generator)
+
+Q(generator)
+
+Q(generator)
+
+Q(get)
+
+Q(getattr)
+
+Q(getter)
+
+Q(globals)
+
+Q(hasattr)
+
+Q(hash)
+
+Q(heap_lock)
+
+Q(heap_unlock)
+
+Q(hex)
+
+Q(id)
+
+Q(imag)
+
+Q(index)
+
+Q(index)
+
+Q(index)
+
+Q(index)
+
+Q(insert)
+
+Q(int)
+
+Q(int)
+
+Q(intersection)
+
+Q(intersection_update)
+
+Q(isalpha)
+
+Q(isalpha)
+
+Q(isdigit)
+
+Q(isdigit)
+
+Q(isdisjoint)
+
+Q(isinstance)
+
+Q(islower)
+
+Q(islower)
+
+Q(isspace)
+
+Q(isspace)
+
+Q(issubclass)
+
+Q(issubset)
+
+Q(issuperset)
+
+Q(isupper)
+
+Q(isupper)
+
+Q(items)
+
+Q(iter)
+
+Q(iterable)
+
+Q(iterator)
+
+Q(iterator)
+
+Q(iterator)
+
+Q(iterator)
+
+Q(iterator)
+
+Q(join)
+
+Q(join)
+
+Q(key)
+
+Q(key)
+
+Q(keys)
+
+Q(keys)
+
+Q(len)
+
+Q(list)
+
+Q(list)
+
+Q(little)
+
+Q(little)
+
+Q(little)
+
+Q(locals)
+
+Q(lower)
+
+Q(lower)
+
+Q(lstrip)
+
+Q(lstrip)
+
+Q(map)
+
+Q(map)
+
+Q(max)
+
+Q(maximum_space_recursion_space_depth_space_exceeded)
+
+Q(micropython)
+
+Q(micropython)
+
+Q(micropython)
+
+Q(min)
+
+Q(module)
+
+Q(next)
+
+Q(object)
+
+Q(object)
+
+Q(oct)
+
+Q(opt_level)
+
+Q(ord)
+
+Q(pack)
+
+Q(pack_into)
+
+Q(pend_throw)
+
+Q(pop)
+
+Q(pop)
+
+Q(pop)
+
+Q(popitem)
+
+Q(pow)
+
+Q(print)
+
+Q(property)
+
+Q(property)
+
+Q(range)
+
+Q(range)
+
+Q(range)
+
+Q(real)
+
+Q(remove)
+
+Q(remove)
+
+Q(replace)
+
+Q(replace)
+
+Q(repr)
+
+Q(reverse)
+
+Q(reverse)
+
+Q(reversed)
+
+Q(reversed)
+
+Q(rfind)
+
+Q(rfind)
+
+Q(rindex)
+
+Q(rindex)
+
+Q(round)
+
+Q(rsplit)
+
+Q(rsplit)
+
+Q(rstrip)
+
+Q(rstrip)
+
+Q(send)
+
+Q(send)
+
+Q(sep)
+
+Q(set)
+
+Q(set)
+
+Q(setattr)
+
+Q(setdefault)
+
+Q(setter)
+
+Q(slice)
+
+Q(slice)
+
+Q(sort)
+
+Q(sorted)
+
+Q(split)
+
+Q(split)
+
+Q(start)
+
+Q(start)
+
+Q(startswith)
+
+Q(startswith)
+
+Q(staticmethod)
+
+Q(staticmethod)
+
+Q(step)
+
+Q(stop)
+
+Q(str)
+
+Q(str)
+
+Q(str)
+
+Q(str)
+
+Q(strip)
+
+Q(strip)
+
+Q(sum)
+
+Q(super)
+
+Q(super)
+
+Q(super)
+
+Q(symmetric_difference)
+
+Q(symmetric_difference_update)
+
+Q(throw)
+
+Q(throw)
+
+Q(to_bytes)
+
+Q(tuple)
+
+Q(tuple)
+
+Q(type)
+
+Q(type)
+
+Q(union)
+
+Q(unpack)
+
+Q(unpack_from)
+
+Q(update)
+
+Q(update)
+
+Q(upper)
+
+Q(upper)
+
+Q(ustruct)
+
+Q(ustruct)
+
+Q(utf_hyphen_8)
+
+Q(utf_hyphen_8)
+
+Q(value)
+
+Q(values)
+
+Q(zip)
+
+Q(zip)
