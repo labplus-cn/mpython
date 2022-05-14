92b59aedb3ff87164ce076f48c8710dc522a6e97
diskman88 <diskman88@163.com>
2022-05-14 10:17:27 +0800

    去掉PSRAM，flash改为8M.

--- port/boards/labplus-Ledong/mpconfigboard.mk
+++ port/boards/labplus-Ledong/mpconfigboard.mk
@@ -1,6 +1,4 @@
 SDKCONFIG += boards/sdkconfig.base
 SDKCONFIG += boards/sdkconfig.240mhz
 SDKCONFIG += boards/sdkconfig.ble
-SDKCONFIG += boards/sdkconfig.spiram
-SDKCONFIG += boards/labplus-Ledong/sdkconfig.spiram
 # SDKCONFIG += boards/mpython/sdkconfig.lwip
