ed1cc19f3f6fa199bb2ea7b891ee29c86211b43a
lijutsang <957790617@qq.com>
2022-11-08 18:01:12 +0800

    up

--- port/boards/mpython-classroom-kit/modules/_boot.py
+++ port/boards/mpython-classroom-kit/modules/_boot.py
@@ -3,6 +3,7 @@ import uos
 from flashbdev import bdev
 from neopixel import NeoPixel
 import machine 
+import time
 # 上电后立即关闭rgb,防止随机灯亮问题
 _rgb = NeoPixel(machine.Pin(17, machine.Pin.OUT), 25, 3, 1,0.1)
 _rgb.write()
