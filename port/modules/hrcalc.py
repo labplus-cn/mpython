8f700ea1856f3695d3fd23476459bce27ec0abd1
lijutsang <957790617@qq.com>
2023-05-12 15:59:33 +0800

    乐动驱动

---
+++ port/modules/hrcalc.py
@@ -0,0 +1,178 @@
+# 25 samples per second (in algorithm.h)
+SAMPLE_FREQ = 25
+#计算HR时取4个样本的移动平均值
+MA_SIZE = 4
+#采样频率
+BUFFER_SIZE = 100
+
+#取平均值
+
+def get_mean(ls):
+    return sum(ls)/len(ls)
+
+def calc_hr_and_spo2(ir_data, red_data):
+
+    ir_mean = int(get_mean(ir_data))
+    x = []
+    for k in ir_data:
+        x.append((k-ir_mean)*-1)
+    m = len(x) - MA_SIZE
+    for i in range(m):
+        x[i] = sum(x[i:i+MA_SIZE]) / MA_SIZE
+
+    #计算阈值
+    n_th = int(get_mean(x))
+    n_th = 30 if n_th < 30 else n_th  #允许的最小值
+    n_th = 60 if n_th > 60 else n_th  #允许的最大值
+
+    ir_valley_locs, n_peaks = find_peaks(x, BUFFER_SIZE, n_th, 4, 15)
+    peak_interval_sum = 0
+    if n_peaks >= 2:
+        for i in range(1, n_peaks):
+            peak_interval_sum += (ir_valley_locs[i] - ir_valley_locs[i-1])
+        peak_interval_sum = int(peak_interval_sum / (n_peaks - 1))
+        hr = int(SAMPLE_FREQ * 60 / peak_interval_sum)
+        hr_valid = True
+    else:
+        hr = -999  #错误值
+        hr_valid = False
+
+    # ---------spo2 血氧浓度---------
+    exact_ir_valley_locs_count = n_peaks
+    for i in range(exact_ir_valley_locs_count):
+        if ir_valley_locs[i] > BUFFER_SIZE:
+            spo2 = -999
+            spo2_valid = False
+            return hr, hr_valid, spo2, spo2_valid
+
+    i_ratio_count = 0
+    ratio = []
+
+    # find max between two valley locations 在两个山谷位置之间找到最大值
+    # and use ratio between AC component of Ir and Red DC component of Ir and Red for SpO2 以及Ir的AC分量与Ir和Red的Red DC分量对于SpO2的使用比率
+    red_dc_max_index = -1
+    ir_dc_max_index = -1
+    for k in range(exact_ir_valley_locs_count-1):
+        red_dc_max = -16777216
+        ir_dc_max = -16777216
+        if ir_valley_locs[k+1] - ir_valley_locs[k] > 3:
+            for i in range(ir_valley_locs[k], ir_valley_locs[k+1]):
+                if ir_data[i] > ir_dc_max:
+                    ir_dc_max = ir_data[i]
+                    ir_dc_max_index = i
+                if red_data[i] > red_dc_max:
+                    red_dc_max = red_data[i]
+                    red_dc_max_index = i
+
+            red_ac = int((red_data[ir_valley_locs[k+1]] - red_data[ir_valley_locs[k]]) * (red_dc_max_index - ir_valley_locs[k]))
+            red_ac = red_data[ir_valley_locs[k]] + int(red_ac / (ir_valley_locs[k+1] - ir_valley_locs[k]))
+            red_ac = red_data[red_dc_max_index] - red_ac  # subtract linear DC components from raw 从原始中减去线性直流分量
+
+            ir_ac = int((ir_data[ir_valley_locs[k+1]] - ir_data[ir_valley_locs[k]]) * (ir_dc_max_index - ir_valley_locs[k]))
+            ir_ac = ir_data[ir_valley_locs[k]] + int(ir_ac / (ir_valley_locs[k+1] - ir_valley_locs[k]))
+            ir_ac = ir_data[ir_dc_max_index] - ir_ac  # subtract linear DC components from raw
+
+            nume = red_ac * ir_dc_max
+            denom = ir_ac * red_dc_max
+            if (denom > 0 and i_ratio_count < 5) and nume != 0:
+                # original cpp implementation uses overflow intentionally.
+                # but at 64-bit OS, Pyhthon 3.X uses 64-bit int and nume*100/denom does not trigger overflow
+                # so using bit operation ( &0xffffffff ) is needed
+                ratio.append(int(((nume * 100) & 0xffffffff) / denom))
+                i_ratio_count += 1
+
+    # choose median value since PPG signal may vary from beat to beat
+    ratio = sorted(ratio)  # sort to ascending order
+    mid_index = int(i_ratio_count / 2)
+
+    ratio_ave = 0
+    if mid_index > 1:
+        ratio_ave = int((ratio[mid_index-1] + ratio[mid_index])/2)
+    else:
+        if len(ratio) != 0:
+            ratio_ave = ratio[mid_index]
+
+    # why 184?
+    # print("ratio average: ", ratio_ave)
+    if ratio_ave > 2 and ratio_ave < 184:
+        # -45.060 * ratioAverage * ratioAverage / 10000 + 30.354 * ratioAverage / 100 + 94.845
+        spo2 = -45.060 * (ratio_ave**2) / 10000.0 + 30.054 * ratio_ave / 100.0 + 94.845
+        spo2_valid = True
+    else:
+        spo2 = -999
+        spo2_valid = False
+
+    return hr, hr_valid, spo2, spo2_valid
+
+
+def find_peaks(x, size, min_height, min_dist, max_num):
+    """
+    Find at most MAX_NUM peaks above MIN_HEIGHT separated by at least MIN_DISTANCE
+    """
+    ir_valley_locs, n_peaks = find_peaks_above_min_height(x, size, min_height, max_num)
+    ir_valley_locs, n_peaks = remove_close_peaks(n_peaks, ir_valley_locs, x, min_dist)
+
+    n_peaks = min([n_peaks, max_num])
+
+    return ir_valley_locs, n_peaks
+
+
+def find_peaks_above_min_height(x, size, min_height, max_num):
+    """
+    Find all peaks above MIN_HEIGHT
+    """
+
+    i = 0
+    n_peaks = 0
+    ir_valley_locs = []  # [0 for i in range(max_num)]
+    while i < size - 1:
+        if x[i] > min_height and x[i] > x[i-1]:  # find the left edge of potential peaks
+            n_width = 1
+            # original condition i+n_width < size may cause IndexError
+            # so I changed the condition to i+n_width < size - 1
+            while i + n_width < size - 1 and x[i] == x[i+n_width]:  # find flat peaks
+                n_width += 1
+            if x[i] > x[i+n_width] and n_peaks < max_num:  # find the right edge of peaks
+                # ir_valley_locs[n_peaks] = i
+                ir_valley_locs.append(i)
+                n_peaks += 1  # original uses post increment
+                i += n_width + 1
+            else:
+                i += n_width
+        else:
+            i += 1
+
+    return ir_valley_locs, n_peaks
+
+
+def remove_close_peaks(n_peaks, ir_valley_locs, x, min_dist):
+    """
+    Remove peaks separated by less than MIN_DISTANCE
+    """
+
+    # should be equal to maxim_sort_indices_descend
+    # order peaks from large to small
+    # should ignore index:0
+    sorted_indices = sorted(ir_valley_locs, key=lambda i: x[i])
+    sorted_indices.reverse()
+
+    # this "for" loop expression does not check finish condition
+    # for i in range(-1, n_peaks):
+    i = -1
+    while i < n_peaks:
+        old_n_peaks = n_peaks
+        n_peaks = i + 1
+        # this "for" loop expression does not check finish condition
+        # for j in (i + 1, old_n_peaks):
+        j = i + 1
+        while j < old_n_peaks:
+            n_dist = (sorted_indices[j] - sorted_indices[i]) if i != -1 else (sorted_indices[j] + 1)  # lag-zero peak of autocorr is at index -1
+            if n_dist > min_dist or n_dist < -1 * min_dist:
+                sorted_indices[n_peaks] = sorted_indices[j]
+                n_peaks += 1  # original uses post increment
+            j += 1
+        i += 1
+
+    sorted_indices[:n_peaks] = sorted(sorted_indices[:n_peaks])
+
+    return sorted_indices, n_peaks
---
+++ port/modules/max30102.py
@@ -0,0 +1,166 @@
+from machine import UART, Pin
+from mpython import *
+import time
+import hrcalc
+
+# i2c 地址
+I2C_WRITE_ADDR = 0xAE
+I2C_READ_ADDR = 0xAF
+
+#设备内部寄存器地址
+REG_INTR_STATUS_1 = 0x00
+REG_INTR_STATUS_2 = 0x01
+
+REG_INTR_ENABLE_1 = 0x02
+REG_INTR_ENABLE_2 = 0x03
+
+REG_FIFO_WR_PTR = 0x04
+REG_OVF_COUNTER = 0x05
+REG_FIFO_RD_PTR = 0x06
+REG_FIFO_DATA = 0x07
+REG_FIFO_CONFIG = 0x08
+
+REG_MODE_CONFIG = 0x09
+REG_SPO2_CONFIG = 0x0A
+REG_LED1_PA = 0x0C
+
+REG_LED2_PA = 0x0D
+REG_PILOT_PA = 0x10
+REG_MULTI_LED_CTRL1 = 0x11
+REG_MULTI_LED_CTRL2 = 0x12
+
+REG_TEMP_INTR = 0x1F
+REG_TEMP_FRAC = 0x20
+REG_TEMP_CONFIG = 0x21
+REG_PROX_INT_THRESH = 0x30
+REG_REV_ID = 0xFE
+REG_PART_ID = 0xFF
+
+class MAX30102():
+    #默认使用引脚X1作为中断引脚，连接模块的INT引脚
+    def __init__(self):
+        # self.i2c = I2C(i2c_id,I2C.MASTER, baudrate=400000)
+        self.ir_D = None
+        self.red_D = None
+        self.i2c = i2c
+        self.address = 0x57
+        # self.interrupt = MPythonPin(15, PinMode.IN)  #设置中断引脚为输入模式
+        self.reset() 
+        # print("intpin: {0}, address: {1}".format(self.address))   
+        time.sleep(1)
+        addr = self.i2c.scan()
+        if self.address not in addr:
+            print('max30102 device not found!\n')
+        else:
+            self.setup()
+        time.sleep(2)
+        print('max30102 device init.\n')
+
+    def _writeReg(self, value, reg):
+        self.i2c.writeto_mem(self.address, reg, value.to_bytes(1, 'little'))
+    
+    def _readReg(self, reg, nbytes=1):
+        return self.i2c.readfrom_mem(self.address, reg, nbytes)
+
+    def shutdown(self):
+        """
+        关闭模块
+        """
+        self._writeReg(0x80, REG_MODE_CONFIG)
+
+    def reset(self):
+        """
+        Reset the device, this will clear all settings,
+        so after running this, run setup() again.
+        """
+        self._writeReg(0x40,  REG_MODE_CONFIG)
+
+    def setup(self, led_mode=0x03):
+        """
+        模块的初始化设置
+        """
+        self._writeReg(0xc0, REG_INTR_ENABLE_1)
+        self._writeReg(0x00, REG_INTR_ENABLE_2)
+        self._writeReg(0x00, REG_FIFO_WR_PTR)
+        self._writeReg(0x00, REG_OVF_COUNTER)
+        self._writeReg(0x00, REG_FIFO_RD_PTR)
+        self._writeReg(0x4f, REG_FIFO_CONFIG)
+        self._writeReg(led_mode, REG_MODE_CONFIG)
+        self._writeReg(0x27, REG_SPO2_CONFIG)
+        self._writeReg(0x24, REG_LED1_PA)
+        self._writeReg(0x24, REG_LED2_PA)
+        self._writeReg(0x7f, REG_PILOT_PA)
+
+
+    def set_config(self, reg, value):
+        self._writeReg(value, reg)
+
+    def read_fifo(self):
+        """
+        读取寄存器的数据
+        """
+        red_led = None
+        ir_led = None
+
+        #从寄存器中读取1个字节的数据
+        reg_INTR1 = self._readReg(REG_INTR_STATUS_1,1)
+        reg_INTR2 = self._readReg(REG_INTR_STATUS_2,1)
+
+        d = self._readReg(REG_FIFO_DATA,6)
+
+        # mask MSB [23:18]
+        red_led = (d[0] << 16 | d[1] << 8 | d[2]) & 0x03FFFF
+        ir_led = (d[3] << 16 | d[4] << 8 | d[5]) & 0x03FFFF
+
+        return red_led, ir_led
+    def read_sequential(self, amount=100):
+        """
+        读取模块上红色LED和红外光LED测量的数据
+        """
+        red_buf = []
+        ir_buf = []
+        for i in range(amount):
+            # while(self.interrupt.read_digital() == 1):
+            #     #等待中断信号
+            #     pass
+            time.sleep_ms(30)
+            red, ir = self.read_fifo()
+
+            red_buf.append(red)
+            ir_buf.append(ir)
+
+        return red_buf, ir_buf
+
+    def measurement(self):
+        time.sleep(0.5)
+        try:
+            print('In the measurement, it takes approximately 30 seconds.\n')
+            print('测量中，将手指放在传感器上保持不动，大约需要30-60秒\n')
+            #采样数据，大约30秒钟
+            red, ir = self.read_sequential(1000)
+            #进行分析
+            ir_avg = []
+            red_avg = []
+            for i in range(37):
+                d = hrcalc.calc_hr_and_spo2(ir[25*i:25*i+100], red[25*i:25*i+100])
+                # print(d)
+                if(d[1] and d[0]<110 and d[0]>40):
+                    ir_avg.append(d[0])
+                if(d[3] and d[2]>80):
+                    red_avg.append(d[2])
+            if(len(ir_avg) >=3):
+                self.ir_D = (sum(ir_avg) - max(ir_avg) - min(ir_avg)) // (len(ir_avg) - 2)
+                self.red_D = (sum(red_avg) - max(red_avg) - min(red_avg)) // (len(red_avg) - 2)
+            else:
+                self.ir_D = (sum(ir_avg)) // len(ir_avg)
+                self.red_D = (sum(red_avg)) // len(red_avg)
+            print(ir_avg)
+            print('Measurement completed.\n')
+            print('测试完成\n')
+            print('****************************\n')
+        except Exception as e:
+            print("An error occurred.\n")
+            print('测量出错。\n')
+            print(e)
+            self.ir_D = None
+            self.red_D = None
