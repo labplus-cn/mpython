018626912edc47db2a7cb35a63509d5ca1517508
lijutsang <957790617@qq.com>
2023-09-07 14:20:59 +0800

    乐动模块更新

--- port/modules/CSK6011A.py
+++ port/modules/CSK6011A.py
@@ -20,7 +20,6 @@ class utf8_gb2312(object):
         utf_byte = char.encode('utf-8')
         r = self.B_S(0, 7296, self.b2i(utf_byte))
         gb2312_byte = self.i2b(r)
-        # print(gb2312_byte)
         return gb2312_byte
 
     def str(self, st):  # 将字符串转化成gb2312
@@ -52,15 +51,44 @@ class utf8_gb2312(object):
 
 class SpeechSynthesis(object):
     def __init__(self,uart_num=1, pin_tx=Pin.P16, pin_rx=Pin.P15):
+        gc.collect()
         self.font_gb2312 = utf8_gb2312()
         self.uart = UART(uart_num, baudrate=115200, tx=pin_tx, rx=pin_rx)
+        self.cache_s = '语音合成'
+        self.cache_byte = b'\xd3\xef\xd2\xf4\xba\xcf\xb3\xc9'
 
     def play(self,s):
-        r = self.font_gb2312.str(str(s))
-        data_len = len(list(r)) + 2 
-        revise_cmd = [0xFD, data_len >> 8, data_len & 0xff, 0x01, 0x00] + list(r)
-        self.uart.write(bytes(revise_cmd))
-        gc.collect()
+        busy_status = self.busy_status()
+        s_len = len(s)
+        wait_ms = 250 * s_len
+        if(not busy_status):
+            print('Synthetic audio ...')
+            if(s == self.cache_s):
+                r = self.cache_byte
+            else:
+                r = self.font_gb2312.str(str(s))
+                self.cache_s = s
+                self.cache_byte = r
+            data_len = len(list(r)) + 2 
+            revise_cmd = [0xFD, data_len >> 8, data_len & 0xff, 0x01, 0x00] + list(r)
+            self.uart.write(bytes(revise_cmd))
+            sleep_ms(wait_ms)
+        else:
+            print('busy status')
+            pass
+
+    def busy_status(self):
+        try:
+            self.uart.write(bytes([0xFD, 0x00, 0x01, 0x21]))
+            sleep_ms(10)
+            if (self.uart.any()):
+                tmp = self.uart.read().decode('utf-8').strip()
+                if(tmp=='O'):
+                    return False
+                elif(tmp=='AN'):
+                    return True
+        except:
+            return True
 
     def release(self):
         self.font_gb2312.__del__()
