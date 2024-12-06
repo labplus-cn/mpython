6041d506d1ccad98ecc28acfc8fc94f29fcec5ff
lijutsang <957790617@qq.com>
2024-12-06 10:32:38 +0800

    mqtt发送延时

--- port/modules/umqtt/robust.py
+++ port/modules/umqtt/robust.py
@@ -1,5 +1,6 @@
 import utime
 from . import simple
+from time import sleep_ms
 
 class MQTTClient(simple.MQTTClient):
 
@@ -27,6 +28,7 @@ class MQTTClient(simple.MQTTClient):
                 self.delay(i)
 
     def publish(self, topic, msg, retain=False, qos=0):
+        sleep_ms(5)
         while 1:
             try:
                 return super().publish(topic, msg, retain, qos)
